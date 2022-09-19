79309b175b53f67426aa673960209f3f2157486f
Shivangi Garg <s43garg@uwaterloo.ca>
2022-09-19 02:01:02 -0400

    Initial Commit

---
+++ HowToCookIt/HowToCookItApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToCookItApp.swift
+//  HowToCookIt
+//
+//  Created by Shivangi on 2022-09-19.
+//
+
+import SwiftUI
+
+@main
+struct HowToCookItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
